379184391f5743c9c3c3bed581e8147f88dd9e53
Jacethejace <110991030+Jacethejace@users.noreply.github.com>
2022-09-09 00:04:40 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,8 @@
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - prints the standard error sandwiched around the standard output
+ * Return:returns 1
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
